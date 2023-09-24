e9c300317aaa90750b72533154b60484c05a3031
Josias maes <jmbaihogaou@gmail.com>
2023-09-24 02:08:54 +0000

    modifing images

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -12,36 +12,38 @@ export default function Home() {
       <div className="spacer" />
       {/* Hello Djamo */}
       <div className="hello-container">
-        <div>
-          <h1>#hellodjamo</h1>
-          <p
-            style={{
-              textAlign: "left",
-              display: "flex",
-              flexDirection: "column",
-              marginBottom: 36,
-            }}
-            className="hello-text"
-          >
-            <span>L'appli pour mieux gérer votre argent.</span>
-            <span>Ouvrez un compte gratuit maintenant en un clic.</span>
-          </p>
-          <div className="buttonn">
-            <Buttony text="Ouvrir un compte gratuit" top={12} left={26} />
-          </div>
-          <div className="form">
-            <select>
-              <option>+225</option>
-            </select>
-            <input type="text" placeholder="Votre numéro de téléphone" />
-            <div>
-              <Buttony text="Ouvrir un compte gratuit" />
+        <div className="hello-container-main">
+          <div>
+            <h1>#hellodjamo</h1>
+            <p
+              style={{
+                textAlign: "left",
+                display: "flex",
+                flexDirection: "column",
+                marginBottom: 36,
+              }}
+              className="hello-text"
+            >
+              <span>L'appli pour mieux gérer votre argent.</span>
+              <span>Ouvrez un compte gratuit maintenant en un clic.</span>
+            </p>
+            <div className="buttonn">
+              <Buttony text="Ouvrir un compte gratuit" top={12} left={26} />
+            </div>
+            <div className="form">
+              <select>
+                <option>+225</option>
+              </select>
+              <input type="text" placeholder="Votre numéro de téléphone" />
+              <div>
+                <Buttony text="Ouvrir un compte gratuit" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="djamo-images">
-          <img src={phone} alt="djamo-phone" />
-          <img src={cart} alt="djamo-phone" className="cart-image" />
+          <div className="djamo-images">
+            <img src={phone} alt="djamo-phone" className="phone-image" />
+            <img src={cart} alt="djamo-phone" className="cart-image" />
+          </div>
         </div>
       </div>
     </div>
